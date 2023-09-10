a2def54848a4d8f11c2caf41c9ded5205612a831
HusnulQolkii <144463373+HusnulQolkii@users.noreply.github.com>
2023-09-10 23:55:18 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new typed("#element",{
+    strings: ["frontend developer" , "yotube", "HUSNUL GANTENG" ,"web developer"],
+    typespeed:100,
+    backspeed:100,
+    backdelay:1000,
+    loop:true
+});
+
